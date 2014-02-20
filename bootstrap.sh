d8fac92e6632288eea3b7eac4e15a8803b06e9eb
Mikko Heikkinen <hawk@biomi.org>
2014-02-20 20:26:19 +0200

    Separating parameters from bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+source /vagrant/bootstrap_parameters.sh
+
 apt-get update
+
+# APACHE
+# ------
+# Prevent from existing custom "000-default.conf"
+rm -rf /etc/apache2/sites-available/000-default.conf
 apt-get install -y apache2
+# Add ServerName to httpd.conf for localhost
+echo "ServerName localhost" > /etc/apache2/httpd.conf
+# Enable "mod_rewrite"
+a2enmod rewrite
+# Finally, restart apache
+service apache2 restart
+
+# DIRECTORIES
+# -----------
 rm -rf /var/www
-mkdir /vagrant/www
+mkdir -p /vagrant/www
 ln -fs /vagrant/www /var/www
---
+++ bootstrap_parameters.sh
@@ -0,0 +1,12 @@
+# https://github.com/Divi/vagrant-bootstrap/blob/master/.vagrant_bootstrap/parameters.sh
+
+# Database
+# --------
+
+DATABASE_NAME="your_database_name"
+# Replace "%" by "localhost" for local user only, 10.0.2.2 for host only or any IP address
+DATABASE_ROOT_HOST="localhost"
+
+# PHP
+# ---
+PHP_TIMEZONE="UTC"
