f01e7832c6bd1d2fa09680ed33aff2e258ee65df
Mikko Heikkinen <hawk@biomi.org>
2014-02-20 22:27:34 +0200

    PHP setup, not working
    http://127.0.0.1:4567/ gives 404

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,6 +22,41 @@ a2enmod rewrite
 # Finally, restart apache
 service apache2 restart
 
+
+# PHP 5.x (last official release)
+# See: https://launchpad.net/~ondrej/+archive/php5
+# ------------------------------------------------
+apt-get install -y libapache2-mod-php5
+# Install "add-apt-repository" binaries
+apt-get install -y python-software-properties
+# Install PHP 5.x
+# Use "ppa:ondrej/php5-oldstable" for old and stable release
+add-apt-repository ppa:ondrej/php5
+# Update repositories
+apt-get update
+
+
+PHP_TIMEZONE="Europe/Helsinki" # temporarily here
+
+
+# PHP tools
+apt-get install -y php5-cli php5-mysql php5-curl php5-mcrypt php5-gd php-pear php5-xdebug php5-intl
+# APC (only with PHP < 5.5.0, use the "opcache" if >= 5.5.0)
+# apt-get install -y php-apc
+# Setting the timezone
+sed 's#;date.timezone\([[:space:]]*\)=\([[:space:]]*\)*#date.timezone\1=\2\"'"$PHP_TIMEZONE"'\"#g' /etc/php5/apache2/php.ini > /etc/php5/apache2/php.ini.tmp
+mv /etc/php5/apache2/php.ini.tmp /etc/php5/apache2/php.ini
+sed 's#;date.timezone\([[:space:]]*\)=\([[:space:]]*\)*#date.timezone\1=\2\"'"$PHP_TIMEZONE"'\"#g' /etc/php5/cli/php.ini > /etc/php5/cli/php.ini.tmp
+mv /etc/php5/cli/php.ini.tmp /etc/php5/cli/php.ini
+# Showing error messages
+sed 's#display_errors = Off#display_errors = On#g' /etc/php5/apache2/php.ini > /etc/php5/apache2/php.ini.tmp
+mv /etc/php5/apache2/php.ini.tmp /etc/php5/apache2/php.ini
+sed 's#display_startup_errors = Off#display_startup_errors = On#g' /etc/php5/apache2/php.ini > /etc/php5/apache2/php.ini.tmp
+mv /etc/php5/apache2/php.ini.tmp /etc/php5/apache2/php.ini
+sed 's#error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT#error_reporting = E_ALL#g' /etc/php5/apache2/php.ini > /etc/php5/apache2/php.ini.tmp
+mv /etc/php5/apache2/php.ini.tmp /etc/php5/apache2/php.ini
+
+
 # Directories
 # ------------------------------------------------
 rm -rf /var/www
